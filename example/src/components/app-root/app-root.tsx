f89bbd98f25042ab0d0e7de8a9a264adaa0b52d5
randy-r <31799713+randy-r@users.noreply.github.com>
2022-10-18 14:04:30 +0300

    Responsiveness styling.

--- example/src/components/app-root/app-root.tsx
+++ example/src/components/app-root/app-root.tsx
@@ -9,11 +9,27 @@ export class AppRoot {
   render() {
     return (
       <div class="root">
-        <header>
+        <nav>
           <h1>
             Ste<span class="highlight">dux</span>
           </h1>
-        </header>
+          <span class="app-links desktop">
+            <stencil-route-link activeClass="active" url="/counter">
+              Counter
+            </stencil-route-link>
+            <stencil-route-link activeClass="active" url="/async">
+              Async
+            </stencil-route-link>
+          </span>
+          <span class="external-links">
+            <a href="https://github.com/randy-r/stedux" target="_blank">
+              github
+            </a>
+            <a href="https://www.npmjs.com/package/stedux" target="_blank">
+              npm
+            </a>
+          </span>
+        </nav>
 
         <main>
           <stencil-router class="flex">
--- example/src/components/ui-jumbo/ui-jumbo.tsx
+++ example/src/components/ui-jumbo/ui-jumbo.tsx
@@ -3,7 +3,6 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: 'ui-jumbo',
   styleUrl: 'ui-jumbo.css',
-  shadow: true,
 })
 export class UiJumbo {
   render() {
